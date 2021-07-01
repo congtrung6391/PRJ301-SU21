455fcda5d4827c9ee673bcb5c333aec757e6b494
khangse <khangnnse140188@fpt.edu.vn>
2021-07-01 17:14:45 +0700

    change1

---
+++ Assignment_2/src/java/DAO/LaptopDAO.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import DBUtils.DBHelper;
+import DTO.LaptopDTO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author khang
+ */
+public class LaptopDAO {
+    public ArrayList<LaptopDTO> SearchLaptop (String name , float minPrice, float maxPrice, int minYear, int maxYear) throws NamingException, SQLException{
+        Connection con = null ;
+        PreparedStatement ps = null;
+        ResultSet rs = null ;
+        ArrayList<LaptopDTO> laptopList = null;
+        try {
+
+            con = DBHelper.makeConnection();
+            if (con != null){
+                String sql = "select LaptopID,Price,Name,CPU,Ram,Screen,Graphic,Disk,OS,Weight,Region,Year "
+                        + "from Laptop "
+                        + "where name like ? and Price > ? and Price < ? and Year > ? and Year < ?";
+                ps = con.prepareStatement(sql);
+                ps.setString(1, "%"+name+"%");
+                ps.setFloat(2, minPrice);
+                ps.setFloat(3, maxPrice);
+                ps.setInt(4, minYear);
+                ps.setInt(5, maxYear);
+                rs = ps.executeQuery();
+                laptopList = new ArrayList();
+                while (rs.next()){
+                    int id = rs.getInt("LaptopID");
+                    float price = rs.getFloat("Price");
+                    String laptopname = rs.getString("Name");
+                    String CPU = rs.getString("CPU");
+                    String ram = rs.getString("Ram");
+                    String Screen = rs.getString("Screen");
+                    String graphic = rs.getString("Graphic");
+                    String disk = rs.getString("Disk");
+                    String OS = rs.getString("OS");
+                    float weight = rs.getFloat("Weight");
+                    String region = rs.getString("Region");
+                    int year = rs.getInt("Year");
+                    LaptopDTO dto = new LaptopDTO(id, laptopname, price, CPU, ram, Screen, graphic, disk, OS, weight, region, year);
+                    laptopList.add(dto);
+                }
+                return laptopList;
+            }
+        } 
+        finally{
+            if (rs != null){
+                rs.close();
+            }
+            if (ps != null){
+                ps.close();
+            }
+            if (con != null){
+                con.close();
+            }
+        }
+        return null;
+    }
+    public boolean CreateLaptop () throws NamingException, SQLException{
+        Connection con = null ;
+        PreparedStatement ps = null;
+        ResultSet rs = null ;
+        try {
+
+            con = DBHelper.makeConnection();
+            if (con != null){
+                String sql = "";
+                
+                while (rs.next()){
+                    
+                }
+            }
+        } 
+        finally{
+            if (rs != null){
+                rs.close();
+            }
+            if (ps != null){
+                ps.close();
+            }
+            if (con != null){
+                con.close();
+            }
+        }
+        return false;
+    }
+}
---
+++ Assignment_2/src/java/Servlet/DispatchServlet.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author khang
+ */
+@WebServlet(name = "DispatchServlet", urlPatterns = {"/DispatchServlet"})
+public class DispatchServlet extends HttpServlet {
+    private final String NULL_CONTROLLER = "nullservlet";
+    private final String LOGIN_PAGE = "Login.jsp";
+    private final String LOGIN_CONTROLLER = "LoginServlet";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String button = request.getParameter("btn");
+        String url = LOGIN_PAGE;
+        try  {
+            if (button == null){
+                url = NULL_CONTROLLER;
+            }
+            if ("Login".equals(button)){
+                url = LOGIN_CONTROLLER;
+            }
+        }
+        finally{
+            request.getRequestDispatcher(url).forward(request, response);
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
